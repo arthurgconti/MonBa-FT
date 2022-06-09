cc701c205a0e7e019a8a78e30d59b54ea5e6fedd
rafa f <rafudzz@gmail.com>
2022-06-09 14:22:04 -0300

    changing some drawer things

--- lib/views/navigation_drawer_widget.dart
+++ lib/views/navigation_drawer_widget.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NavigationDrawerWidget extends StatelessWidget {
+class NavigationDrawerWidget extends ListView {
   final padding = EdgeInsets.symmetric(horizontal: 20);
   @override
   Widget build(BuildContext context){
     return Drawer(
-      child: Container(
-        color: Color.fromARGB(190, 188, 224, 56),
-        child: ListView(
-          padding: padding,
-          children: <Widget> [
-            const SizedBox(height: 48),
-            buildMenuItem(
-              text: 'Colocar Algo',
-              icon: Icons.people,
-            )
+      backgroundColor: Color.fromARGB(255, 154, 182, 55),
+      child: Expanded(
+          child: ListView(children: [
+            Card(
+              color: Color.fromARGB(255, 180, 180, 180),
+                child: ListTile(
+                    title: Text("Minhas Notificações"),
+                    leading: const Icon(Icons.circle_notifications_rounded))),
+                    const Divider(),
           ],
         ),
       ),
@@ -23,15 +22,33 @@ class NavigationDrawerWidget extends StatelessWidget {
   }
 }
 
-Widget buildMenuItem({
-  required String text,
-  required IconData icon,
-}) {
-  final color = Colors.black;
+      // child: Container(
+      //   color: Color.fromARGB(190, 188, 224, 56),
+      //   child: ListView(
+      //     padding: padding,
+      //     children: Card(
+      //           child: ListTile(
+      //               title: Text(widget.bathroomName),
+      //               leading: const Icon(Icons.location_on))),
 
-  return ListTile(
-    leading: Icon(icon, color: color),
-    title: Text(text, style: TextStyle(color: color)),
-    onTap: () {},
-  );
-}
+
+
+          // children: <Widget> [
+          //   const SizedBox(height: 48),
+            // buildMenuItem(
+            //   text: 'Colocar Algo',
+            //   icon: Icons.people,
+            // )
+
+// Widget buildMenuItem({
+//   required String text,
+//   required IconData icon,
+// }) {
+//   final color = Colors.black;
+
+//   return ListTile(
+//     leading: Icon(icon, color: color),
+//     title: Text(text, style: TextStyle(color: color)),
+//     onTap: () {},
+//   );
+// }
