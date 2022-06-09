5df4a00f5ef5fae9bea5af30dfee32a27d40683e
Arthur Conti <arthurgallotticonti@gmail.com>
2022-06-09 15:40:45 -0300

    cleaning import

--- lib/provider/firestore_provider.dart
+++ lib/provider/firestore_provider.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dio/dio.dart';
 import 'package:monba_ft/enum/statusEnum.dart';
 import 'package:monba_ft/model/banheiroPA.dart';
 
